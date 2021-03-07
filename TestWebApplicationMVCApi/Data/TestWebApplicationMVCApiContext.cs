b5ede593fd57a1e1c3f75cd6693be98855200bff
steven <stevenss>
2021-03-07 23:23:53 +0100

    Add project files.

---
+++ TestWebApplicationMVCApi/Data/TestWebApplicationMVCApiContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TestWebApplicationMVCApi.Models;
+
+namespace TestWebApplicationMVCApi.Data
+{
+    public class TestWebApplicationMVCApiContext : DbContext
+    {
+        public TestWebApplicationMVCApiContext (DbContextOptions<TestWebApplicationMVCApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TestWebApplicationMVCApi.Models.User> User { get; set; }
+
+        public DbSet<TestWebApplicationMVCApi.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ TestWebApplicationMVCApi/Migrations/20210306210715_Initial.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TestWebApplicationMVCApi.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movie",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    ReleaseDate = table.Column<DateTime>(nullable: false),
+                    Genre = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false),
+                    Rating = table.Column<string>(nullable: true),
+                    UserID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movie", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    username = table.Column<string>(nullable: true),
+                    password = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movie");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
